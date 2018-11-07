9ad8edceb09d24c728bdf6534efb9b3dfb89613c
5256 SpareParts <cchsrobochargers@gmail.com>
2018-11-06 21:42:52 -0500

    Made it a superior Autonomous
    --NGL

--- Autonomous.c
+++ Autonomous.c
@@ -70,7 +70,139 @@ void spin(float deg, int speed, bool hold)
 		wait1Msec(10);
 }
 
-void pre_auton()
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+
+//Wait for Press--------------------------------------------------
+void waitForPress()
+{
+	while(nLCDButtons == 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+//Wait for Release------------------------------------------------
+void waitForRelease()
+{
+	while(nLCDButtons != 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+task main()
+{
+	//Declare count variable to keep track of our choice
+	int count = 0;
+
+	//------------- Beginning of User Interface Code ---------------
+	//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Loop while center button is not pressed
+	while(nLCDButtons != centerButton)
+	{
+		//Switch case that allows the user to choose from 4 different options
+		switch(count){
+		case 0:
+			//Display first choice
+			displayLCDCenteredString(0, "Autonomous 1");
+			displayLCDCenteredString(1, "<		 Enter		>");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count = 3;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+			}
+			break;
+		case 1:
+			//Display second choice
+			displayLCDCenteredString(0, "Autonomous 2");
+			displayLCDCenteredString(1, "<		 Enter		>");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+			}
+			break;
+		case 2:
+			//Display third choice
+			displayLCDCenteredString(0, "Autonomous 3");
+			displayLCDCenteredString(1, "<		 Enter		>");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+			}
+			break;
+		case 3:
+			//Display fourth choice
+			displayLCDCenteredString(0, "Autonomous 4");
+			displayLCDCenteredString(1, "<		 Enter		>");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count = 0;
+			}
+			break;
+		default:
+			count = 0;
+			break;
+		}
+	}
+	//------------- End of User Interface Code ---------------------
+
+	//------------- Beginning of Robot Movement Code ---------------
+	//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Switch Case that actually runs the user choice
+	switch(count){
+	case 0:
+		//If count = 0, run the code correspoinding with choice 1
+		displayLCDCenteredString(0, "BlueFlag");
+		displayLCDCenteredString(1, "is running!");
+		wait1Msec(2000);						// Robot waits for 2000 milliseconds
+
+		void pre_auton()
 {
 	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
 	// running between Autonomous and Driver controlled modes. You will need to
@@ -88,21 +220,325 @@ void pre_auton()
 	slaveMotor(leftMotor2, leftMotor1);
 	slaveMotor(rightMotor2, rightMotor1);
 }
-
+		task autonomous()
+		{
+			// Move reverse at full power for 3 seconds
+			motor[rightMotor] = -127;			// Motor on port2 is run at full (-127) power reverse
+			motor[leftMotor]	= -127;			// Motor on port3 is run at full (-127) power reverse
+			wait1Msec(3000);							// Robot runs previous code for 3000 milliseconds before moving on
+		}
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
-/*                              Autonomous Task                              */
+/*                              User Control Task                            */
 /*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
+/*  This task is used to control your robot during the user control phase of */
 /*  a VEX Competition.                                                       */
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while(1 == 1)
+	{
+		//Driving Motor Control
+		motor[leftMotor1] = vexRT[Ch3] / 2;
+		motor[rightMotor1] = vexRT[Ch2] / 2;
+
+		//Lift Control
+		if(vexRT[Btn6U] == 1)
+		{
+			motor[liftMotor] = 200;
+		}
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[liftMotor] = -40;
+		}
+		else
+		{
+			motor[liftMotor] = 0;
+		}
+
+		//claw control
+		//if(vexRT[Btn5U] == 1)       	//If Button 6U is pressed...
+		//{
+		//motor[wristMotor] = 127;  		//...close the gripper.
+		//}
+		//else if(vexRT[Btn5D] == 1)  	//Else, if button 6D is pressed...
+		//{
+		//motor[wristMotor] = -127; 		//...open the gripper.
+		//}
+		//else                      		//Else (neither button is pressed)...
+		//{
+		//motor[wristMotor] = 0;    		//...stop the gripper.
+		//}
+		//delay(10);
+	}
+}
+		break;
+	case 1:
+		//If count = 1, run the code correspoinding with choice 2
+		displayLCDCenteredString(0, "BlueNoFlag");
+		displayLCDCenteredString(1, "is running!");
+		wait1Msec(2000);						// Robot waits for 2000 milliseconds
+
+		void pre_auton()
+{
+	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
+	// running between Autonomous and Driver controlled modes. You will need to
+	// manage all user created tasks if set to false.
+	bStopTasksBetweenModes = true;
+
+	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
+	// used by the competition include file, for example, you might want
+	// to display your team name on the LCD in this function.
+	// bDisplayCompetitionStatusOnLcd = false;
+
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+	//The Motors are slaved because we had to change to
+	slaveMotor(leftMotor2, leftMotor1);
+	slaveMotor(rightMotor2, rightMotor1);
+}
+		task autonomous()
+		{
+			// Move reverse at full power for 3 seconds
+			motor[rightMotor] = -127;			// Motor on port2 is run at full (-127) power reverse
+			motor[leftMotor]	= -127;			// Motor on port3 is run at full (-127) power reverse
+			wait1Msec(3000);							// Robot runs previous code for 3000 milliseconds before moving on
+		}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while(1 == 1)
+	{
+		//Driving Motor Control
+		motor[leftMotor1] = vexRT[Ch3] / 2;
+		motor[rightMotor1] = vexRT[Ch2] / 2;
+
+		//Lift Control
+		if(vexRT[Btn6U] == 1)
+		{
+			motor[liftMotor] = 200;
+		}
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[liftMotor] = -40;
+		}
+		else
+		{
+			motor[liftMotor] = 0;
+		}
+
+		//claw control
+		//if(vexRT[Btn5U] == 1)       	//If Button 6U is pressed...
+		//{
+		//motor[wristMotor] = 127;  		//...close the gripper.
+		//}
+		//else if(vexRT[Btn5D] == 1)  	//Else, if button 6D is pressed...
+		//{
+		//motor[wristMotor] = -127; 		//...open the gripper.
+		//}
+		//else                      		//Else (neither button is pressed)...
+		//{
+		//motor[wristMotor] = 0;    		//...stop the gripper.
+		//}
+		//delay(10);
+	}
+}
+
+		break;
+	case 2:
+		//If count = 2, run the code correspoinding with choice 3
+		displayLCDCenteredString(0, "RedFlag");
+		displayLCDCenteredString(1, "is running!");
+		wait1Msec(2000);						// Robot waits for 2000 milliseconds
+
+				void pre_auton()
+{
+	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
+	// running between Autonomous and Driver controlled modes. You will need to
+	// manage all user created tasks if set to false.
+	bStopTasksBetweenModes = true;
+
+	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
+	// used by the competition include file, for example, you might want
+	// to display your team name on the LCD in this function.
+	// bDisplayCompetitionStatusOnLcd = false;
+
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+	//The Motors are slaved because we had to change to
+	slaveMotor(leftMotor2, leftMotor1);
+	slaveMotor(rightMotor2, rightMotor1);
+}
+		task autonomous()
+		{
+		//Turn right for 3 seconds
+		motor[rightMotor] = -63;			// Motor on port2 is run at half power reverse
+		motor[leftMotor]	= 63;				// Motor on port3 is run at half power forward
+		wait1Msec(3000);							// Robot runs previous code for 3000 milliseconds before moving on
+		}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while(1 == 1)
+	{
+		//Driving Motor Control
+		motor[leftMotor1] = vexRT[Ch3] / 2;
+		motor[rightMotor1] = vexRT[Ch2] / 2;
+
+		//Lift Control
+		if(vexRT[Btn6U] == 1)
+		{
+			motor[liftMotor] = 200;
+		}
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[liftMotor] = -40;
+		}
+		else
+		{
+			motor[liftMotor] = 0;
+		}
+
+		//claw control
+		//if(vexRT[Btn5U] == 1)       	//If Button 6U is pressed...
+		//{
+		//motor[wristMotor] = 127;  		//...close the gripper.
+		//}
+		//else if(vexRT[Btn5D] == 1)  	//Else, if button 6D is pressed...
+		//{
+		//motor[wristMotor] = -127; 		//...open the gripper.
+		//}
+		//else                      		//Else (neither button is pressed)...
+		//{
+		//motor[wristMotor] = 0;    		//...stop the gripper.
+		//}
+		//delay(10);
+	}
+}
+
+		break;
+	case 3:
+		//If count = 3, run the code correspoinding with choice 4
+		displayLCDCenteredString(0, "RedNoFlag");
+		displayLCDCenteredString(1, "is running!");
+		wait1Msec(2000);						// Robot waits for 2000 milliseconds
+
+				void pre_auton()
+{
+	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
+	// running between Autonomous and Driver controlled modes. You will need to
+	// manage all user created tasks if set to false.
+	bStopTasksBetweenModes = true;
+
+	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
+	// used by the competition include file, for example, you might want
+	// to display your team name on the LCD in this function.
+	// bDisplayCompetitionStatusOnLcd = false;
+
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+	//The Motors are slaved because we had to change to
+	slaveMotor(leftMotor2, leftMotor1);
+	slaveMotor(rightMotor2, rightMotor1);
+}
+		task autonomous()
+		{
+
+		//Turn left for 3 seconds
+		motor[rightMotor] = 63;				// Motor on port2 is run at half power forward
+		motor[leftMotor]	= -63;			// Motor on port3 is run at half power reverse
+		wait1Msec(3000);							// Robot runs previous code for 3000 milliseconds before moving on
+		}
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while(1 == 1)
+	{
+		//Driving Motor Control
+		motor[leftMotor1] = vexRT[Ch3] / 2;
+		motor[rightMotor1] = vexRT[Ch2] / 2;
+
+		//Lift Control
+		if(vexRT[Btn6U] == 1)
+		{
+			motor[liftMotor] = 200;
+		}
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[liftMotor] = -40;
+		}
+		else
+		{
+			motor[liftMotor] = 0;
+		}
+
+		//claw control
+		//if(vexRT[Btn5U] == 1)       	//If Button 6U is pressed...
+		//{
+		//motor[wristMotor] = 127;  		//...close the gripper.
+		//}
+		//else if(vexRT[Btn5D] == 1)  	//Else, if button 6D is pressed...
+		//{
+		//motor[wristMotor] = -127; 		//...open the gripper.
+		//}
+		//else                      		//Else (neither button is pressed)...
+		//{
+		//motor[wristMotor] = 0;    		//...stop the gripper.
+		//}
+		//delay(10);
+	}
+}
+		break;
+	default:
+		displayLCDCenteredString(0, "You Idiot!");
+		displayLCDCenteredString(1, "it broke");
+		break;
+	}
+	//------------- End of Robot Movement Code -----------------------
+}
+
 task autonomous()
 {
-	if((SensorValue[nearflag] == 0) && (SensorValue[blue] == 0))
-	{
 		//moves even with cap
 		move(12, 127, false); //moves 12 inches forward
 		spin(-90, 127, false); //turns 90 degrees left
@@ -120,27 +556,6 @@ task autonomous()
 		motor[liftMotor] =-50;
 		wait1Msec(900);
 		motor[liftMotor] =0; //the lift is lowered
-	}
-	else if ((SensorValue[nearflag] == 0) && (SensorValue[blue] == 1)) //Inverse --Red Side
-	{
-		//moves even with cap
-		move(12, 127, false); //moves 12 inches forward
-		spin(90, 127, false); //turns 90 degrees right
-
-		move(6, 127, false); //robot moves up to the cap and lifts it, flips it, lowers cascade
-		motor[liftMotor] =-50;
-		wait1Msec(1000);
-		motor[liftMotor] =0;
-
-		move(-12, 127, false); //robot moves backward
-		spin(-90, 127, false); //turns left
-		move(36, 127, false); //moves forward 3 feet and turns flag
-
-		move(-6, 127, false); //robot moves back
-		motor[liftMotor] =-50;
-		wait1Msec(900);
-		motor[liftMotor] =0; //the lift is lowered
-	}
 }
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
